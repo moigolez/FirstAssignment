ad7e9f7933b22d995bdcbbb75aee29188c73f630
MOILENOVO\moigo <mgolez7185@southwestuniversity.edu>
2021-01-21 11:13:59 -0700

    This is my first assignment

---
+++ Module 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module_1
+{
+	class Program
+	{
+		static void Main()
+		{
+			ConcatanationOfStrings("my first string", " My second string");
+		}
+		static void ConcatanationOfStrings(string parameter1, string parameter2)
+		{
+			string result = parameter1 + "" +parameter2;
+
+			Console.WriteLine(result);
+		}
+	}
+}
